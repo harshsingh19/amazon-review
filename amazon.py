30f222f5f891daf4a423aed4f595b6e06295caa7
Harsh <harsh.singh659@gmail.com>
2020-03-27 19:29:10 +0530

    Add files via upload
    More number of columns in data

--- amazon.py
+++ amazon.py
@@ -9,16 +9,20 @@ Created on Wed Mar 20 20:28:30 2019
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
-from wordcloud import WordCloud,STOPWORDS
+#from wordcloud import WordCloud,STOPWORDS
 import matplotlib.pyplot as plt
 import time
 import os
 import requests
-from commentsentiment import main_seniment
+#from commentsentiment import main_seniment
 review =[]
 rating = []
+review_name = []
 clean_rating = []
+review_title = []
+clean_review_title =[]
 clean_review = []
+clean_review_name = []
 clean_timeing =[]
 timeing = []
 positive=[]
@@ -32,7 +36,7 @@ def cleanhtml(raw_html):
     return cleantext
 
 def review_taker(soup):
-    global review,clean_review;
+    global review,clean_review
     count = 0
     for i in soup.findAll("div",{"class":"a-row a-spacing-small review-data"}):
         review.append(str(i))
@@ -42,8 +46,23 @@ def review_taker(soup):
         clean_review.append(cleanhtml(i))
     review = []
 
+def review_name_taker(soup):
+    global review_name,clean_review_name
+    for i in soup.findAll("div",{"class":"a-profile-content"}):
+        review_name.append(str(i))
+    review_name = review_name[2:]
+    for i in review_name:
+        clean_review_name.append(cleanhtml(i))
+    review_name = []
+def review_title_taker(soup):
+    global review_title,clean_review_title
+    for i in soup.findAll("a",{"data-hook":"review-title"}):
+        review_title.append(str(i))
+    for i in review_title:
+        clean_review_title.append(cleanhtml(i))
+    review_title = []
 def rating_taker(soup):
-    global rating,clean_rating;  
+    global rating,clean_rating  
     for i in soup.findAll("span",{ "class":"a-icon-alt"}):
         rating.append(str(i))
     rating = rating[3:13]
@@ -59,7 +78,7 @@ def time_review(soup):
         clean_timeing.append(cleanhtml(i))
     timeing=[]
 def html_data_returner(url):
-    headers_Get = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
+    headers_Get = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.5',
         'Accept-Encoding': 'gzip, deflate',
@@ -83,9 +102,6 @@ def review_pagefinder(soup):
             print("Count of no of review page ",count)
         url = "https://www.amazon.in"+review_link
         soup = html_data_returner(url)
-        review_taker(soup)
-        rating_taker(soup)
-        time_review(soup)
         return soup
     except:
         pass
@@ -103,15 +119,23 @@ def review_getter(soup):
             review_taker(soup)
             rating_taker(soup)
             time_review(soup)
+            review_name_taker(soup)
+            review_title_taker(soup)
     except:
         pass        
 def rating_equalizer():
-    global clean_rating ,clean_review,positive,negative,netural
+    global clean_rating ,clean_review,clean_review_name
     if len(clean_review) != len(clean_rating):
         clean_rating = clean_rating[:len(clean_review)]
+        clean_review_name = clean_review_name[:len(clean_review)]
 def csv_saver():
-    global clean_rating ,clean_review,timeing
-    pairs = {'Clean_Rating': clean_rating, 'Clean_Review': clean_review, 'Time':clean_timeing}
+    global clean_rating ,clean_review,timeing,clean_review_name
+    pairs = {'Reviewer Name':clean_review_name,'Clean_Rating': clean_rating, 'Clean_Review': clean_review, 'Time':clean_timeing, "Review title":clean_review_title}
+    print("Count of reviewr Name :",len(clean_review_name))
+    print("Count of reviewr raiting :",len(clean_rating))
+    print("Count of reviewr review :",len(clean_review))
+    print("Count of reviewr time :",len(clean_timeing))
+    print("Count of reviewr time :",len(clean_review_title))
     df = pd.DataFrame.from_dict(pairs)
     if os.path.exists("Data"):
         if os.path.exists("Data/Data.csv"):
@@ -134,7 +158,7 @@ def attribute_getter(soup):
     description=description+hidden_description
     title = cleanhtml(title)
     over_all_rating = cleanhtml(over_all_rating)
-    over_all_rating = over_all_rating[2:over_all_rating.index("review")+6]
+    over_all_rating = over_all_rating[2:32]
     description = cleanhtml(description)
     attribute_saver(title,over_all_rating,description)
 def attribute_saver(title,over_all_rating,description):
@@ -310,7 +334,8 @@ def main(url):
         soup_review = review_pagefinder(soup)
         review_getter(soup_review)
         rating_equalizer()
-        df = csv_saver()
-        split_negative_positive_netural()
-        checkval = main_seniment(df)
-        return checkval
+        csv_saver()
+        #split_negative_positive_netural()
+        #checkval = main_seniment(df)
+        #return checkval
+main("https://www.amazon.in/Tim-Hawk-Mini-Brush-Cleaning/dp/B01AQJSBA0/ref=sr_1_3?pf_rd_p=677d5279-a152-45a4-9b75-9419c0c3d7cd&pf_rd_r=ATXNR63KTQ56KAWV066J&qid=1585311304&s=kitchen&sr=1-3&srs=21246718031")
